123f17b2d77cb0c5c447906d8a3d82add4c2c6f2
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-05-17 22:16:43 +0530

    Updated the help center

--- src/operations/help.rs
+++ src/operations/help.rs
@@ -13,7 +13,8 @@ pub fn help_center() {
   println!("{}", "                  🛠️ Available Commands:".bright_green());
   println!("{}", "  cvert [URL]              Shorten a URL".bright_green());
   println!("{}", "  show all                 List all shortened URLs".bright_green());
-  println!("{}", "  search -ol/-sl [CODE]    Retrieve a shortened URL by code".bright_green());
+  println!("{}", "  search -ol/-sl [CODE]    Retrieve a URL by code".bright_green());
+  println!("{}", "  list -ol/-sl [CODE]      Retrieve a URL by specific characters".bright_green());
   println!("{}", "  delete [CODE]            Delete a shortened URL by code".bright_green());
   println!("{}", "  help                     Show this help message".bright_green());
   println!("{}", "----------------------------------------------------------------------".cyan());
